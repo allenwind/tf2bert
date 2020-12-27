5ec756a3d4793cfdbd1c01c77e94cb45ea9aecae
allenwind <allenwind@foxmail.com>
2020-12-27 23:32:34 +0800

    Add files structure

---
+++ tflayers.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow.keras.layers import *
+
+class SinCosPositionEmbedding(Layer):
+    pass
+
+class LearnablePositionEmbedding(Layer):
+    pass
+
+
+class MultiHeadAttention(Layer):
+    pass
+
+
+class FFN(Layer):
+    pass
