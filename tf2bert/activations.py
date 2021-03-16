a9a6a7e80858d6b4a7e2a1040badec531752936d
allenwind <allenwind@foxmail.com>
2021-03-16 23:37:22 +0800

    Update activations

--- tf2bert/activations.py
+++ tf2bert/activations.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
-approximate = False
+# 默认使用erf的实现
+gelu_approximate = False
 
 def gelu_erf(x):
     return 0.5 * x * (1.0 + tf.math.erf(x / tf.sqrt(2.0)))
@@ -9,7 +10,7 @@ def gelu_tanh(x):
     cdf = 0.5 * (1.0 + tf.tanh((tf.sqrt(2 / 3.141592653589793) * (x + 0.044715 * tf.pow(x, 3)))))
     return x * cdf
 
-gelu = gelu_tanh if approximate else gelu_erf
+gelu = gelu_tanh if gelu_approximate else gelu_erf
 
 def leaky_relu(x, alpha=0.2):
     return tf.nn.leaky_relu(x, alpha=alpha)
@@ -19,8 +20,8 @@ def mish(x):
     """
     return x * tf.math.tanh(tf.math.softplus(x))
 
-tf.keras.utils.get_custom_objects()['gelu'] = gelu
-tf.keras.utils.get_custom_objects()['gelu_erf'] = gelu_erf
-tf.keras.utils.get_custom_objects()['gelu_tanh'] = gelu_tanh
-tf.keras.utils.get_custom_objects()['leaky_relu'] = leaky_relu
-tf.keras.utils.get_custom_objects()['mish'] = mish
+tf.keras.utils.get_custom_objects()["gelu"] = gelu
+tf.keras.utils.get_custom_objects()["gelu_erf"] = gelu_erf
+tf.keras.utils.get_custom_objects()["gelu_tanh"] = gelu_tanh
+tf.keras.utils.get_custom_objects()["leaky_relu"] = leaky_relu
+tf.keras.utils.get_custom_objects()["mish"] = mish
