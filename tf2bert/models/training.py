66a8879fc64bc63a0d1a403d6550cb3fa8df8c5d
allenwind <allenwind@foxmail.com>
2021-04-13 16:03:09 +0800

    Add GradientPenalty model

--- tf2bert/models/training.py
+++ tf2bert/models/training.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 class AdversarialTraining(tf.keras.Model):
     """对抗训练器，像tf.keras.Model一样使用，
-    这里实现的是Fast Gradient Method。"""
+    这里实现的是Fast Gradient Method"""
 
     def compile(self, optimizer, loss, metrics, eps=1.0, layer_name="embedding"):
         super(AdversarialTraining, self).compile(
@@ -32,4 +32,45 @@ class AdversarialTraining(tf.keras.Model):
         results = super(AdversarialTraining, self).train_step(data)
         # 删除Embedding矩阵上的扰动
         embeddings.assign_sub(delta)
+        return results
+
+    def compute_delta(self):
+        pass
+
+class GradientPenalty(tf.keras.Model):
+    """在train_step中实现梯度惩罚的逻辑"""
+
+    def compile(self, eps, layer_name="embedding", **kwargs):
+        super(GradientPenalty, self).compile(**kwargs)
+        self.eps = eps
+        # Embedding层的名字
+        self.layer_name = layer_name
+
+    def gradient_penalty(self, x, y):
+        # 计算gradient penalty
+        embedding_layer = self.get_layer(self.layer_name)
+        embeddings = embedding_layer.embeddings
+        with tf.GradientTape() as tape:
+            tape.watch(embeddings)
+            y_pred = self(x, training=True)
+            loss = self.compiled_loss(y, y_pred)
+
+        grads = tape.gradient(loss, embeddings)
+        gp = tf.reduce_sum(tf.square(grads))
+        return gp
+
+    def train_step(self, data):
+        # 计算二阶梯度
+        x, y = data
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = self.compiled_loss(y, y_pred)
+            gp = self.gradient_penalty(x, y)
+            total_loss = loss + 0.5 * self.eps * gp
+
+        grads = tape.gradient(total_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        self.compiled_metrics.update_state(y, y_pred)
+        results = {m.name: m.result() for m in self.metrics}
+        results.update(gp=gp)
         return results
