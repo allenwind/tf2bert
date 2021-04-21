Fix attention mask

tf2bert/models/mask.py
@@ -3,11 +3,12 @@ import tensorflow as tf
 # 在tf2bert/tests下有这两种掩码的numpy验证
 
 class LMMaskMixIn:
-    """计算下三角Mask，用于语言模型。"""
+    """计算下三角Mask，用于语言模型。这里使用MixIn的写法，
+    通过继承即可扩展mask。"""
 
     def compute_attention_mask(self, inputs=None):
         if self.attention_mask is None:
-            def lm_mask(s):
+            def compute_mask(s):
                 seq_len = tf.shape(s)[1]
                 indices = tf.range(0, seq_len)
                 # tf.expand_dims ~ [None]
@@ -16,12 +17,11 @@ class LMMaskMixIn:
                 return - (1 - mask[None, None]) * 1e12
 
             self.attention_mask = self.build_layer(
-                inputs=inputs,
+                inputs=self.inputs[0],
                 layer=Lambda,
-                function=lm_mask,
+                function=compute_mask,
                 name="Attention-LM-Mask"
             )
-
         return self.attention_mask
 
 class UniLMMaskMixIn:
@@ -30,7 +30,7 @@ class UniLMMaskMixIn:
 
     def compute_attention_mask(self, inputs=None):
         if self.attention_mask is None:
-            def unilm_mask(s):
+            def compute_mask(s):
                 indices = tf.cumsum(s, axis=1)
                 mask = indices[:, None, :] <= indices[:, :, None]
                 mask = tf.cast(mask, tf.float32)
@@ -39,10 +39,9 @@ class UniLMMaskMixIn:
             self.attention_mask = self.build_layer(
                 inputs=self.inputs[1],
                 layer=Lambda,
-                function=unilm_mask,
+                function=compute_mask,
                 name="Attention-UniLM-Mask"
             )
-
         return self.attention_mask
 
 def install_attention_mask(TransformerModel, mask):
@@ -53,7 +52,6 @@ def install_attention_mask(TransformerModel, mask):
         def __init__(self, *args, **kwargs):
             super(LanguageModelTransformer, self).__init__(*args, **kwargs)
             self.with_mlm = self.with_mlm or True
-
 
     class UniLMTransformer(UniLMMaskMixIn, TransformerModel):
 