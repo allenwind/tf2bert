e48e737fcebe6a44635f3bf867860e445bfcf19a
allenwind <allenwind@foxmail.com>
2021-06-20 06:19:16 +0800

    Add importing logo

--- tf2bert/__init__.py
+++ tf2bert/__init__.py
@@ -17,3 +17,12 @@ from . import metrics
 from . import optimizers
 from . import utils
 from . import preprocessing
+
+print("""
+  _    __ ___  _
+ | |  / _|__ \| |             | |
+ | |_| |_   ) | |__   ___ _ __| |
+ | __|  _| / /| '_ \ / _ \ '__| __|
+ | |_| |  / /_| |_) |  __/ |  | |
+  \__|_| |____|_.__/ \___|_|   \__|
+""")
