b20bc036df254a9fcc3a76182fb5cef15cfa68a7
allenwind <allenwind@foxmail.com>
2021-06-21 16:20:01 +0800

    Add wasserstein distance

--- tf2bert/math/distance.py
+++ tf2bert/math/distance.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from scipy import optimize
 import scipy.spatial as spatial
 
 def manhattan(x, y):
@@ -43,3 +44,32 @@ def mahalanobis_distance(obs, X, center="zero"):
     # 计算 obs 到 center 的 Mahalanobis distance
     d = spatial.distance.mahalanobis(obs, center, icov)
     return d
+
+def wasserstein_distance(p, q, C):
+    """Wasserstein距离计算方法，
+    p.shape=(m,)
+    q.shape=(n,)
+    C.shape=(m,n)
+    p q满足归一性概率化
+    """
+    p = np.array(p)
+    q = np.array(q)
+    A_eq = []
+    for i in range(len(p)):
+        A = np.zeros_like(C)
+        A[i,:] = 1.0
+        A_eq.append(A.reshape((-1,)))
+    for i in range(len(q)):
+        A = np.zeros_like(C)
+        A[:,i] = 1.0
+        A_eq.append(A.reshape((-1,)))
+    A_eq = np.array(A_eq)
+    b_eq = np.concatenate([p, q], axis=0)
+    C = np.array(C).reshape((-1,))
+    return optimize.linprog(
+        c=C,
+        A_eq=A_eq[:-1],
+        b_eq=b_eq[:-1],
+        method="interior-point",
+        options={"cholesky":False, "sym_pos":True}
+    ).fun
