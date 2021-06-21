95168a1d0031f27afd12e5f6e53ac42636b37c7a
allenwind <allenwind@foxmail.com>
2021-06-21 16:09:08 +0800

    Add mahalanobis distance

--- tf2bert/math/distance.py
+++ tf2bert/math/distance.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import scipy.spatial as spatial
 
 def manhattan(x, y):
     # L1
@@ -22,3 +23,23 @@ def hamming(x, y):
 
 def py_minkowski(x, y, p=2):
     return np.sum(np.abs(x-y)**p) ** (1 / p)
+
+def mahalanobis_distance(obs, X, center="zero"):
+    # mahalanobis distance of obs to X
+    # wiki:
+    # https://en.wikipedia.org/wiki/Mahalanobis_distance
+
+    # 计算协方差矩阵
+    cov = np.cov(X.T)
+
+    # 计算数据集的中心
+    if center == "zero":
+        center = np.zeros(cov.shape[1])
+    else:
+        center = np.mean(X, axis=0)
+
+    # 矩阵的逆不一定存在，这里使用矩阵的伪逆
+    icov = np.linalg.pinv(cov)
+    # 计算 obs 到 center 的 Mahalanobis distance
+    d = spatial.distance.mahalanobis(obs, center, icov)
+    return d
