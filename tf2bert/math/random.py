2d86cf96be8b4c3a4e1aa6bc8659b9584df669d1
allenwind <allenwind@foxmail.com>
2021-07-20 22:27:17 +0800

    Add inverse_variance_weighted_sum

--- tf2bert/math/random.py
+++ tf2bert/math/random.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 def reduce_moments():
@@ -8,3 +9,23 @@ def reduce_variance(x, axis=None, keepdims=False, name="reduce_variance"):
     diff = x - m
     sdiff = tf.square(diff)
     return tf.reduce_mean(sdiff, axis=axis, keepdims=keepdims)
+
+def inverse_variance_weighted_sum(vs, return_weights=False):
+    """逆方差加权平均"""
+    mu = np.mean(vs, axis=1, keepdims=True)
+    var = 1 / (vs.shape[1] - 1) * np.sum(np.square(vs - mu), axis=1, keepdims=True)
+    ivar = 1 / var
+    w = ivar / np.sum(ivar, axis=0, keepdims=True)
+    s = np.sum(vs * w, axis=0)
+    if return_weights:
+        return s, w
+    return s
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    x = np.cumsum(np.random.normal(size=(10, 512)), axis=-1)
+    y, w = inverse_variance_weighted_sum(x, return_weights=True)
+    plt.plot(x.T, color="blue")
+    plt.plot(y, color="red")
+    print(w)
+    plt.show()
