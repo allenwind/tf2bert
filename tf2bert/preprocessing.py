f2c2166ca48e321dcc029f8464af45890d5f7bb3
allenwind <allenwind@foxmail.com>
2021-08-17 21:07:00 +0800

    Remove unused snippets

--- tf2bert/preprocessing.py
+++ tf2bert/preprocessing.py
@@ -38,20 +38,3 @@ def batch_to_array(*inputs):
         return xs[0]
     return xs
 
-def batch_paded_generator(X, y, label2id, tokenizer, batch_size, epochs):
-    X = tokenizer.transform(X)
-    y = batch_tags2ids(y, label2id)
-    iterations = (len(X) // batch_size + 1) * epochs * batch_size
-    X = itertools.cycle(X)
-    y = itertools.cycle(y)
-    gen = zip(X, y)
-    batch_X = []
-    batch_y = []
-    for _ in range(iterations):
-        sample_x, sample_y = next(gen)
-        batch_X.append(sample_x)
-        batch_y.append(sample_y)
-        if len(batch_X) == batch_size:
-            yield batch_pad(batch_X), batch_pad(batch_y)
-            batch_X = []
-            batch_y = []
